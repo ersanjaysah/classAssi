cac3e8dbc14157246ad7db066f8d637ce3d4ccf8
sanjay <ssah6694@gmail.com>
2022-03-10 23:32:11 +0530

    [sanjay]Added:Leap year .gitignore also

---
+++ Class2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date9Assignment
+{
+    public class Class2
+    {
+        public static bool isLeapYear(int year)
+        {
+            if((year%400==0)||(year%100!=0&&year%4==0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool isleap(bool isleapyear,int year)
+        {
+            if (isleapyear)
+            {
+                Console.WriteLine(year + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine(year + "  is not a leap year");
+            }
+            return isleapyear;
+            
+        }
+
+    }
+}
