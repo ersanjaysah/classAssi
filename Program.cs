483d2a97d86f599f2a20c206b81e77a457bceea3
sanjay <ssah6694@gmail.com>
2022-03-10 18:46:55 +0530

    sanjay added program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Date9Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter number you want to flip");
+            int tossnumber=int.Parse(Console.ReadLine());
+            class1.flip(tossnumber);
+
+            Console.WriteLine("please enter year");
+            int year = int.Parse(Console.ReadLine());
+            bool isleapyear=Class2.isLeapYear(year);
+            Class2.isleap(isleapyear, year);
+        }
+    }
+}
